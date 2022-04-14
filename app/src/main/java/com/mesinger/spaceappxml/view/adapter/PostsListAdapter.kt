fea1282aad310d9639c621203bcb0b2e6c428801
Zvonimir Mesinger <zvonimir.mesinger@gmail.com>
2022-04-14 15:52:32 +0200

    RecyclerView

---
+++ app/src/main/java/com/mesinger/spaceappxml/view/adapter/PostsListAdapter.kt
@@ -0,0 +1,23 @@
+package com.mesinger.spaceappxml.view.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PostsListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class PostsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
--- app/src/main/java/com/mesinger/spaceappxml/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/mesinger/spaceappxml/viewmodel/HomeViewModel.kt
@@ -1,6 +1,20 @@
 package com.mesinger.spaceappxml.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mesinger.spaceappxml.service.model.UserImage
 
 class HomeViewModel : ViewModel() {
+
+    private var postsList: MutableLiveData<ArrayList<UserImage>> = MutableLiveData()
+
+    private fun getPostsObserver(): MutableLiveData<ArrayList<UserImage>>{
+        return postsList
+    }
+
+
+    private fun getPosts(){
+
+    }
+
 }
