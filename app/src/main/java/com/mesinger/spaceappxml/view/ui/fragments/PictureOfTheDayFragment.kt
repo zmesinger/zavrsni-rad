aec9861ca2ebd75b99c41cd099774d2172cbcfe5
Zvonimir Mesinger <zvonimir.mesinger@gmail.com>
2022-06-02 19:47:31 +0200

    Implement retrofit

--- app/src/main/java/com/mesinger/spaceappxml/view/ui/fragments/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/mesinger/spaceappxml/view/ui/fragments/PictureOfTheDayFragment.kt
@@ -1,12 +1,18 @@
 package com.mesinger.spaceappxml.view.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.mesinger.spaceappxml.R
 import com.mesinger.spaceappxml.databinding.FragmentPictureOfTheDayBinding
+import com.mesinger.spaceappxml.service.repository.api.RetrofitInstance
+import retrofit2.HttpException
+import java.io.IOException
 
 
 class PictureOfTheDayFragment : Fragment() {
@@ -22,10 +28,40 @@ class PictureOfTheDayFragment : Fragment() {
 
         binding = FragmentPictureOfTheDayBinding.inflate(inflater, container, false)
 
+
+        lifecycleScope.launchWhenCreated {
+            val response = try {
+                RetrofitInstance.apodApi.getData()
+            }catch (e: IOException){
+                Log.d("PictureOfTheDay", "IOException, onCreate")
+                return@launchWhenCreated
+            }catch (e: HttpException){
+                Log.d("PictureOfTheDay", "HttpException, onCreate")
+                return@launchWhenCreated
+            }
+
+            if(response.isSuccessful && response.body() != null){
+                binding.apply {
+                    titleTextView.text = response.body()!!.title
+                    authorTextView.text = response.body()!!.copyright
+                    dateTextView.text = response.body()!!.date
+                    actualDescriptionTextView.text = response.body()!!.explanation
+                    Glide.with(requireContext())
+                        .load(response.body()!!.hdurl)
+                        .into(picOfTheDayImageView)
+                }
+            }else{
+                Log.d("PictureOfTheDay", "Response not successful")
+            }
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
+
+
 }
